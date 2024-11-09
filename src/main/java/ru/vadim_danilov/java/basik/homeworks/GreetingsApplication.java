38c7bfe6c68069328e08e8d1ee95a0352cd3ab42
Vadim Danilov <vsdanilov2018@mail.ru>
2024-11-09 15:48:56 +0400

    init

---
+++ src/main/java/ru/vadim_danilov/java/basik/homeworks/GreetingsApplication.java
@@ -0,0 +1,7 @@
+package ru.vadim_danilov.java.basik.homeworks;
+
+public class GreetingsApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
